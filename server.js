97e07f6aa867b7b1269ae08e7c9931296cca3b6f
ghaniz <ghanizahid@hotmail.com>
2023-10-19 20:05:52 -0400

    day 5 custom migrations, helper method with express endpoints, advanced filtering

---
+++ server.js
@@ -0,0 +1,26 @@
+// DEPENDENCIES
+const express = require('express')
+const app = express()
+const { Sequelize } = require('sequelize')
+
+// CONFIGURATION / MIDDLEWARE
+require('dotenv').config()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// ROOT
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome to the Tour API on Ghani\'s computer'
+    })
+})
+
+// CONTROLLERS 
+const bandsController = require('./controllers/bands_controller')
+app.use('/bands', bandsController)
+
+
+// LISTEN
+app.listen(process.env.PORT, () => {
+    console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
+})
